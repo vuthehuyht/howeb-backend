929e57f1bf92f40c72bf04e292f771a39dfdfd4c
Huy Vu The <vuthehu.hus@gmail.com>
2021-12-23 21:16:33 +0700

    Initial commit

---
+++ src/main/java/ums/luci/vn/emailservice/HowebBackendApplication.java
@@ -0,0 +1,13 @@
+package ums.luci.vn.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ums/luci/vn/emailservice/HowebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ums.luci.vn.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
